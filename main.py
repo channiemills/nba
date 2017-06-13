8fcbf7269033cb6749213016c4214d3fa554bf68
Chantel A. Miller <cmiller20@gmail.com>
2017-06-13 02:14:41 -0500

    moving season record total into own function and changing main output to be df with each record before game starts

--- main.py
+++ main.py
@@ -38,37 +38,46 @@ df_each_file = (pd.read_csv(f) for f in game_files)
 source = pd.concat(df_each_file, ignore_index=True)
 
 
-## Single function for column transforms## 
-# Order by Game Date, Time
-source['Date'] = pd.to_datetime(source['Date'])
-source['Start (ET)'] = pd.to_datetime(source['Start (ET)'], format='%I:%M %p').dt.time
-source.sort_values(['Date', 'Start (ET)'], ascending=[1, 1], inplace=True)
+def column_transforms(source):
+    """
+    Performs column transformations on basketball reference columns
+    :param source: Data frame of game data from bball ref
+    :return: Data frame with transformed columns
+    """
+    # Order by game date, time
+    source['Date'] = pd.to_datetime(source['Date'])
+    source['Start (ET)'] = pd.to_datetime(source['Start (ET)'], format='%I:%M %p').dt.time
+    source.sort_values(['Date', 'Start (ET)'], ascending=[1, 1], inplace=True)
 
-# Add GameID column to the beginning
-source.insert(0, 'GameID', range(1, len(source['Date'])+1))
+    # Add GameID column to the beginning
+    source.insert(0, 'GameID', range(1, len(source['Date'])+1))
 
-# Rename Columns
-source = source.rename(columns={
-    'Visitor/Neutral': 'Away',
-    'Home/Neutral': 'Home',
-    'Unnamed: 7': 'Overtime',
-    'Start (ET)': 'Start_EST'
-})
+    # Rename Columns
+    source = source.rename(columns={
+        'Visitor/Neutral': 'Away',
+        'Home/Neutral': 'Home',
+        'Unnamed: 7': 'Overtime',
+        'Start (ET)': 'Start_EST'
+    })
 
-# Drop Unnecessary Columns
+    # Drop Unnecessary Columns
+    source = source.drop(['Unnamed: 6', 'Notes'], 1)
 
-source = source.drop(['Unnamed: 6', 'Notes'], 1)
+    # Update Overtime Column w/ 1 and 0
+    source['Overtime'].fillna(0, inplace=True)
+    source.loc[source['Overtime'] == 'OT', 'Overtime'] = 1
 
-# Update Overtime Column w/ 1 and 0
-source['Overtime'].fillna(0, inplace=True)
-source.loc[source['Overtime'] == 'OT', 'Overtime'] = 1
+    return source
 
-## / end function ##
-
-# Identify Winner #
+source = column_transforms(source)
 
 
 def who_won(row):
+    """
+    Determines which team won based on point totals.
+    :param row: Row in nba data frame it is applied to
+    :return: Returns strings 'Home' or 'Away'
+    """
     if row['PTS.1'] > row['PTS']:
         winner = 'Home'
     else:
@@ -80,7 +89,13 @@ source['Winner'] = source.apply(who_won, axis=1)
 
 
 def point_diff(row):
+    """
+    Calculates point differential for a game
+    :param row: Row in nba data frame function is applied to
+    :return: Returns differnce in points between home and away team
+    """
     return abs(row['PTS.1']-row['PTS'])
+
 
 source['PT_Diff'] = source.apply(point_diff, axis=1)
 
@@ -94,8 +109,15 @@ def reform_df(source_df, empty_df, x, y, z):
     """
     Creates new df using index of home and away team and strings 'Home' and 'Away'
     Used to build game level data for each team.
+    Note: Need to pass column indices for columns containing team names because couldn't make it a string variable
+    :param source_df: Source nba data frame
+    :param empty_df: Empty data frame to populate for either home or away team
+    :param x: Populates team. Expects column of home team (4) if reforming home df or away if reforming away (6) df
+    :param y: String either 'Home' or 'Away'
+    :param z: Populates opponent. Expects column of away team (6) if reforming home df or home if reforming away (4) df
+    :return: A data frame with relevant columns for either the home team or away team for each game. 
     """
-    for i, r in enumerate(source_df.itertuples(),1):
+    for i, r in enumerate(source_df.itertuples(), 1):
         empty_df = empty_df.append({'GameID': r.GameID,
                                     'Team': r[x],
                                     'H/A': y,
@@ -109,8 +131,6 @@ def reform_df(source_df, empty_df, x, y, z):
 
 df_away = reform_df(source, df_away, 4, 'Away', 6)
 df_home = reform_df(source, df_home, 6, 'Home', 4)
-
-# print df_away.head(), df_home.head()
 
 # Add W/L column
 
@@ -128,7 +148,7 @@ df_away['W/L'] = df_away.apply(lambda row: win_or_loss(row, 'Away'), axis=1)
 df_home['W/L'] = df_home.apply(lambda row: win_or_loss(row, 'Home'), axis=1)
 
 # Combine into one df to get all games
-df = pd.concat([df_away,df_home])
+df = pd.concat([df_away, df_home])
 
 ## Function for result df cleanup ## 
 df.sort_values(['GameID', 'W/L'], ascending=[1, 0], inplace=True)
@@ -138,7 +158,8 @@ df.sort_values(['GameID', 'W/L'], ascending=[1, 0], inplace=True)
 df['PT_Diff'] = df['PT_Diff'].where(df['W/L'] == 1, -df['PT_Diff'])
 
 
-# Add fields needed for record PCT #
+# Add fields needed for record PCT. This will be different if I want the season total or game level. #
+# May want to group by seasons one day
 
 df['Wins'] = (df['W/L']).groupby(df['Team']).cumsum()-1  # subtracting one to calc record coming into the game
 df['GP'] = df.groupby('Team').cumcount()  # got rid of +1 to not count first instance
@@ -165,22 +186,32 @@ df = df.round({'PCT': 3, 'Opponent_PCT': 3})
 
 # Reorder columns for clarity
 
-df = df[['GameID', 'Date', 'Start_EST', 'H/A', 'Team', 'W/L', 'Overtime', 'PT_Diff', 'Wins', 'GP', 'PCT', 'Opponent', 'Opponent_PCT']]
+df = df[['GameID', 'Date', 'Start_EST', 'H/A', 'Team', 'W/L', 'Overtime',
+         'PT_Diff', 'Wins', 'GP', 'PCT', 'Opponent', 'Opponent_PCT']]
 
-df.to_csv('result.csv')
+#df.to_csv('result.csv')
 
 ## /end function ## 
 
 # Sanity check
 
-## sanity check function? ## 
+## If I want to know who won I can just make a function that does different column calculations ##
 
-wins = df[['Team', 'Wins', 'GP', 'PCT', 'Opponent', 'Opponent_PCT']][df['GP'] == 82]
-wins.sort_values(['PCT','Team'], ascending=[0, 1], inplace=True)
-wins['Losses'] = wins['GP']-wins['Wins']
-check = wins[['Team', 'Wins', 'Losses', 'PCT']]
+def season_totals(df):
 
-## / end function ## 
+    wins = df.copy()
+    wins['Wins'] = (wins['W/L']).groupby(wins['Team']).cumsum()  # Changing wins to account for wins after game played
+    wins['GP'] = wins.groupby('Team').cumcount()+1  # Adding 1 to include initial instance
+    wins['PCT'] = ((wins['W/L']).groupby(wins['Team']).cumsum()/(wins.groupby('Team').cumcount()+1))
+    wins['Losses'] = wins['GP']-wins['Wins']
+    check = wins[['Team', 'Wins', 'Losses', 'PCT']][wins['GP'] == 82]
+    check.sort_values(['PCT', 'Team'], ascending=[0, 1], inplace=True)
+    check = check.round({'PCT': 3})
+    return check
+
+# wins = df[['Team', 'Wins', 'GP', 'PCT', 'Opponent', 'Opponent_PCT']][df['GP'] == 82]
+
+check = season_totals(df)
 
 execution_time = timeit.default_timer() - start_time
 print check.head(10)
--- performance.py
+++ performance.py
@@ -12,9 +12,9 @@ def vs500teams(df, team, type):
     if type == 'above':
         above500_wins = teamdf[teamdf.Opponent_PCT >= 0.500].sum()['W/L']
         above500_games = teamdf[teamdf.Opponent_PCT >= 0.500].count()['W/L']
-        return above500_wins/above500_games
+        return round(above500_wins/above500_games, 3)
     elif type == 'below':
         below500_wins = teamdf[teamdf.Opponent_PCT < 0.500].sum()['W/L']
         below500_games = teamdf[teamdf.Opponent_PCT < 0.500].count()['W/L']
-        return below500_wins/below500_games
+        return round(below500_wins/below500_games, 3)
 
